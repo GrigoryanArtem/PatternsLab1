c9e4782aac0aee20f58e68821aa23940c91f7311
Artem Grigoryan <artem.thebest@mail.ru>
2015-10-23 12:23:35 +0300

    Added France pizza

---
+++ Decorator/Pizzas/FrancePizza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Pizza
+{
+    class FrancePizza : Pizza
+    {
+        private const double COST = 33.50;
+        private const uint CALORIES = 800;
+
+        public FrancePizza()
+        {
+            _information = "France Pizza";
+        }
+
+        public override double GetCost()
+        {
+            return COST;
+        }
+
+        public override uint GetCalories()
+        {
+            return CALORIES;
+        }
+    }
+}
