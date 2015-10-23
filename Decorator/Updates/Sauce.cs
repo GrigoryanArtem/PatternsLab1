f6a5f02bba561ca10ffbe4f8523aece13e56c06a
Artem Grigoryan <artem.thebest@mail.ru>
2015-10-23 12:26:57 +0300

    Added Sauce

---
+++ Decorator/Updates/Sauce.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Updates
+{
+    class Sauce : Update
+    {
+        private const string INFO = " + Sauce";
+        private const double COST = 1.0;
+        private const uint CALORIES = 0;
+        public Sauce(Pizza.Pizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public override string GetInfo()
+        {
+            return _pizza.GetInfo() + INFO;
+        }
+
+        public override double GetCost()
+        {
+            return _pizza.GetCost() + COST;
+        }
+
+        public override uint GetCalories()
+        {
+            return _pizza.GetCalories() + CALORIES;
+        }
+    }
+}
