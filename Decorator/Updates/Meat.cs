3b0e19b0257f179e5cace5d745a64702b45f820d
Artem Grigoryan <artem.thebest@mail.ru>
2015-10-23 12:17:06 +0300

    Added meat and pine

---
+++ Decorator/Updates/Meat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Updates
+{
+    class Meat : Update
+    {
+        private const string INFO = " + Meat";
+        private const double COST = 5.75;
+        private const uint CALORIES = 50;
+        public Meat(Pizza.Pizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public override string GetInfo()
+        {
+            return _pizza.GetInfo() + INFO;
+        }
+
+        public override double GetCost()
+        {
+            return _pizza.GetCost() + COST;
+        }
+
+        public override uint GetCalories()
+        {
+            return _pizza.GetCalories() + CALORIES;
+        }
+    }
+}
---
+++ Decorator/Updates/Pine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Updates
+{
+    class Pine : Update
+    {
+        private const string INFO = " + Pine";
+        private const double COST = 1.50;
+        private const uint CALORIES = 5;
+        public Pine(Pizza.Pizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public override string GetInfo()
+        {
+            return _pizza.GetInfo() + INFO;
+        }
+
+        public override double GetCost()
+        {
+            return _pizza.GetCost() + COST;
+        }
+
+        public override uint GetCalories()
+        {
+            return _pizza.GetCalories() + CALORIES;
+        }
+    }
+}
